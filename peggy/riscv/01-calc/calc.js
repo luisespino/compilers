afca004a28ceba6e33d00879eb9dc2101a9c0e54
Luis Espino <66042898+luisespino@users.noreply.github.com>
2024-09-02 18:47:00 -0600

    update calc

--- peggy/riscv/01-calc/calc.js
+++ peggy/riscv/01-calc/calc.js
@@ -1,11 +1,9 @@
-// import model 
-import { StartRules, SyntaxError, parse } from './parser.mjs'
 
 let x;
 
 document.getElementById("x").addEventListener("keydown", function(e) {
     if (e.key === "Enter") {
         x = document.getElementById("x").value;
-        document.getElementById("log").innerHTML = parse(x);
+        document.getElementById("log").innerHTML = PEG.parse(x);
     }
 });
