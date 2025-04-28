54d5ba12837ab1811b0e761249168a82b6a15e7e
Luis Espino <66042898+luisespino@users.noreply.github.com>
2025-04-28 14:09:09 -0600

    update grammar

---
+++ antlr-csharp/06-calc-arm64/.antlr/GrammarParser.java
@@ -0,0 +1,545 @@
+// Generated from /home/luisespino/Documents/GitHub/compilers/antlr-csharp/06-calc-arm64/Grammar.g4 by ANTLR 4.13.1
+import org.antlr.v4.runtime.atn.*;
+import org.antlr.v4.runtime.dfa.DFA;
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.misc.*;
+import org.antlr.v4.runtime.tree.*;
+import java.util.List;
+import java.util.Iterator;
+import java.util.ArrayList;
+
+@SuppressWarnings({"all", "warnings", "unchecked", "unused", "cast", "CheckReturnValue"})
+public class GrammarParser extends Parser {
+	static { RuntimeMetaData.checkVersion("4.13.1", RuntimeMetaData.VERSION); }
+
+	protected static final DFA[] _decisionToDFA;
+	protected static final PredictionContextCache _sharedContextCache =
+		new PredictionContextCache();
+	public static final int
+		T__0=1, T__1=2, T__2=3, T__3=4, T__4=5, T__5=6, T__6=7, ID=8, NUM=9, WS=10;
+	public static final int
+		RULE_p = 0, RULE_s = 1, RULE_e = 2, RULE_t = 3, RULE_f = 4;
+	private static String[] makeRuleNames() {
+		return new String[] {
+			"p", "s", "e", "t", "f"
+		};
+	}
+	public static final String[] ruleNames = makeRuleNames();
+
+	private static String[] makeLiteralNames() {
+		return new String[] {
+			null, "'='", "'+'", "'-'", "'*'", "'/'", "'('", "')'"
+		};
+	}
+	private static final String[] _LITERAL_NAMES = makeLiteralNames();
+	private static String[] makeSymbolicNames() {
+		return new String[] {
+			null, null, null, null, null, null, null, null, "ID", "NUM", "WS"
+		};
+	}
+	private static final String[] _SYMBOLIC_NAMES = makeSymbolicNames();
+	public static final Vocabulary VOCABULARY = new VocabularyImpl(_LITERAL_NAMES, _SYMBOLIC_NAMES);
+
+	/**
+	 * @deprecated Use {@link #VOCABULARY} instead.
+	 */
+	@Deprecated
+	public static final String[] tokenNames;
+	static {
+		tokenNames = new String[_SYMBOLIC_NAMES.length];
+		for (int i = 0; i < tokenNames.length; i++) {
+			tokenNames[i] = VOCABULARY.getLiteralName(i);
+			if (tokenNames[i] == null) {
+				tokenNames[i] = VOCABULARY.getSymbolicName(i);
+			}
+
+			if (tokenNames[i] == null) {
+				tokenNames[i] = "<INVALID>";
+			}
+		}
+	}
+
+	@Override
+	@Deprecated
+	public String[] getTokenNames() {
+		return tokenNames;
+	}
+
+	@Override
+
+	public Vocabulary getVocabulary() {
+		return VOCABULARY;
+	}
+
+	@Override
+	public String getGrammarFileName() { return "Grammar.g4"; }
+
+	@Override
+	public String[] getRuleNames() { return ruleNames; }
+
+	@Override
+	public String getSerializedATN() { return _serializedATN; }
+
+	@Override
+	public ATN getATN() { return _ATN; }
+
+	public GrammarParser(TokenStream input) {
+		super(input);
+		_interp = new ParserATNSimulator(this,_ATN,_decisionToDFA,_sharedContextCache);
+	}
+
+	@SuppressWarnings("CheckReturnValue")
+	public static class PContext extends ParserRuleContext {
+		public PContext(ParserRuleContext parent, int invokingState) {
+			super(parent, invokingState);
+		}
+		@Override public int getRuleIndex() { return RULE_p; }
+	 
+		public PContext() { }
+		public void copyFrom(PContext ctx) {
+			super.copyFrom(ctx);
+		}
+	}
+	@SuppressWarnings("CheckReturnValue")
+	public static class ProgramContext extends PContext {
+		public List<SContext> s() {
+			return getRuleContexts(SContext.class);
+		}
+		public SContext s(int i) {
+			return getRuleContext(SContext.class,i);
+		}
+		public ProgramContext(PContext ctx) { copyFrom(ctx); }
+	}
+
+	public final PContext p() throws RecognitionException {
+		PContext _localctx = new PContext(_ctx, getState());
+		enterRule(_localctx, 0, RULE_p);
+		int _la;
+		try {
+			_localctx = new ProgramContext(_localctx);
+			enterOuterAlt(_localctx, 1);
+			{
+			setState(11); 
+			_errHandler.sync(this);
+			_la = _input.LA(1);
+			do {
+				{
+				{
+				setState(10);
+				s();
+				}
+				}
+				setState(13); 
+				_errHandler.sync(this);
+				_la = _input.LA(1);
+			} while ( _la==ID );
+			}
+		}
+		catch (RecognitionException re) {
+			_localctx.exception = re;
+			_errHandler.reportError(this, re);
+			_errHandler.recover(this, re);
+		}
+		finally {
+			exitRule();
+		}
+		return _localctx;
+	}
+
+	@SuppressWarnings("CheckReturnValue")
+	public static class SContext extends ParserRuleContext {
+		public SContext(ParserRuleContext parent, int invokingState) {
+			super(parent, invokingState);
+		}
+		@Override public int getRuleIndex() { return RULE_s; }
+	 
+		public SContext() { }
+		public void copyFrom(SContext ctx) {
+			super.copyFrom(ctx);
+		}
+	}
+	@SuppressWarnings("CheckReturnValue")
+	public static class AssignContext extends SContext {
+		public TerminalNode ID() { return getToken(GrammarParser.ID, 0); }
+		public EContext e() {
+			return getRuleContext(EContext.class,0);
+		}
+		public AssignContext(SContext ctx) { copyFrom(ctx); }
+	}
+
+	public final SContext s() throws RecognitionException {
+		SContext _localctx = new SContext(_ctx, getState());
+		enterRule(_localctx, 2, RULE_s);
+		try {
+			_localctx = new AssignContext(_localctx);
+			enterOuterAlt(_localctx, 1);
+			{
+			setState(15);
+			match(ID);
+			setState(16);
+			match(T__0);
+			setState(17);
+			e(0);
+			}
+		}
+		catch (RecognitionException re) {
+			_localctx.exception = re;
+			_errHandler.reportError(this, re);
+			_errHandler.recover(this, re);
+		}
+		finally {
+			exitRule();
+		}
+		return _localctx;
+	}
+
+	@SuppressWarnings("CheckReturnValue")
+	public static class EContext extends ParserRuleContext {
+		public EContext(ParserRuleContext parent, int invokingState) {
+			super(parent, invokingState);
+		}
+		@Override public int getRuleIndex() { return RULE_e; }
+	 
+		public EContext() { }
+		public void copyFrom(EContext ctx) {
+			super.copyFrom(ctx);
+		}
+	}
+	@SuppressWarnings("CheckReturnValue")
+	public static class SumresContext extends EContext {
+		public Token op;
+		public EContext e() {
+			return getRuleContext(EContext.class,0);
+		}
+		public TContext t() {
+			return getRuleContext(TContext.class,0);
+		}
+		public SumresContext(EContext ctx) { copyFrom(ctx); }
+	}
+	@SuppressWarnings("CheckReturnValue")
+	public static class PassTContext extends EContext {
+		public TContext t() {
+			return getRuleContext(TContext.class,0);
+		}
+		public PassTContext(EContext ctx) { copyFrom(ctx); }
+	}
+
+	public final EContext e() throws RecognitionException {
+		return e(0);
+	}
+
+	private EContext e(int _p) throws RecognitionException {
+		ParserRuleContext _parentctx = _ctx;
+		int _parentState = getState();
+		EContext _localctx = new EContext(_ctx, _parentState);
+		EContext _prevctx = _localctx;
+		int _startState = 4;
+		enterRecursionRule(_localctx, 4, RULE_e, _p);
+		int _la;
+		try {
+			int _alt;
+			enterOuterAlt(_localctx, 1);
+			{
+			{
+			_localctx = new PassTContext(_localctx);
+			_ctx = _localctx;
+			_prevctx = _localctx;
+
+			setState(20);
+			t(0);
+			}
+			_ctx.stop = _input.LT(-1);
+			setState(27);
+			_errHandler.sync(this);
+			_alt = getInterpreter().adaptivePredict(_input,1,_ctx);
+			while ( _alt!=2 && _alt!=org.antlr.v4.runtime.atn.ATN.INVALID_ALT_NUMBER ) {
+				if ( _alt==1 ) {
+					if ( _parseListeners!=null ) triggerExitRuleEvent();
+					_prevctx = _localctx;
+					{
+					{
+					_localctx = new SumresContext(new EContext(_parentctx, _parentState));
+					pushNewRecursionContext(_localctx, _startState, RULE_e);
+					setState(22);
+					if (!(precpred(_ctx, 2))) throw new FailedPredicateException(this, "precpred(_ctx, 2)");
+					setState(23);
+					((SumresContext)_localctx).op = _input.LT(1);
+					_la = _input.LA(1);
+					if ( !(_la==T__1 || _la==T__2) ) {
+						((SumresContext)_localctx).op = (Token)_errHandler.recoverInline(this);
+					}
+					else {
+						if ( _input.LA(1)==Token.EOF ) matchedEOF = true;
+						_errHandler.reportMatch(this);
+						consume();
+					}
+					setState(24);
+					t(0);
+					}
+					} 
+				}
+				setState(29);
+				_errHandler.sync(this);
+				_alt = getInterpreter().adaptivePredict(_input,1,_ctx);
+			}
+			}
+		}
+		catch (RecognitionException re) {
+			_localctx.exception = re;
+			_errHandler.reportError(this, re);
+			_errHandler.recover(this, re);
+		}
+		finally {
+			unrollRecursionContexts(_parentctx);
+		}
+		return _localctx;
+	}
+
+	@SuppressWarnings("CheckReturnValue")
+	public static class TContext extends ParserRuleContext {
+		public TContext(ParserRuleContext parent, int invokingState) {
+			super(parent, invokingState);
+		}
+		@Override public int getRuleIndex() { return RULE_t; }
+	 
+		public TContext() { }
+		public void copyFrom(TContext ctx) {
+			super.copyFrom(ctx);
+		}
+	}
+	@SuppressWarnings("CheckReturnValue")
+	public static class PassFContext extends TContext {
+		public FContext f() {
+			return getRuleContext(FContext.class,0);
+		}
+		public PassFContext(TContext ctx) { copyFrom(ctx); }
+	}
+	@SuppressWarnings("CheckReturnValue")
+	public static class MuldivContext extends TContext {
+		public Token op;
+		public TContext t() {
+			return getRuleContext(TContext.class,0);
+		}
+		public FContext f() {
+			return getRuleContext(FContext.class,0);
+		}
+		public MuldivContext(TContext ctx) { copyFrom(ctx); }
+	}
+
+	public final TContext t() throws RecognitionException {
+		return t(0);
+	}
+
+	private TContext t(int _p) throws RecognitionException {
+		ParserRuleContext _parentctx = _ctx;
+		int _parentState = getState();
+		TContext _localctx = new TContext(_ctx, _parentState);
+		TContext _prevctx = _localctx;
+		int _startState = 6;
+		enterRecursionRule(_localctx, 6, RULE_t, _p);
+		int _la;
+		try {
+			int _alt;
+			enterOuterAlt(_localctx, 1);
+			{
+			{
+			_localctx = new PassFContext(_localctx);
+			_ctx = _localctx;
+			_prevctx = _localctx;
+
+			setState(31);
+			f();
+			}
+			_ctx.stop = _input.LT(-1);
+			setState(38);
+			_errHandler.sync(this);
+			_alt = getInterpreter().adaptivePredict(_input,2,_ctx);
+			while ( _alt!=2 && _alt!=org.antlr.v4.runtime.atn.ATN.INVALID_ALT_NUMBER ) {
+				if ( _alt==1 ) {
+					if ( _parseListeners!=null ) triggerExitRuleEvent();
+					_prevctx = _localctx;
+					{
+					{
+					_localctx = new MuldivContext(new TContext(_parentctx, _parentState));
+					pushNewRecursionContext(_localctx, _startState, RULE_t);
+					setState(33);
+					if (!(precpred(_ctx, 2))) throw new FailedPredicateException(this, "precpred(_ctx, 2)");
+					setState(34);
+					((MuldivContext)_localctx).op = _input.LT(1);
+					_la = _input.LA(1);
+					if ( !(_la==T__3 || _la==T__4) ) {
+						((MuldivContext)_localctx).op = (Token)_errHandler.recoverInline(this);
+					}
+					else {
+						if ( _input.LA(1)==Token.EOF ) matchedEOF = true;
+						_errHandler.reportMatch(this);
+						consume();
+					}
+					setState(35);
+					f();
+					}
+					} 
+				}
+				setState(40);
+				_errHandler.sync(this);
+				_alt = getInterpreter().adaptivePredict(_input,2,_ctx);
+			}
+			}
+		}
+		catch (RecognitionException re) {
+			_localctx.exception = re;
+			_errHandler.reportError(this, re);
+			_errHandler.recover(this, re);
+		}
+		finally {
+			unrollRecursionContexts(_parentctx);
+		}
+		return _localctx;
+	}
+
+	@SuppressWarnings("CheckReturnValue")
+	public static class FContext extends ParserRuleContext {
+		public FContext(ParserRuleContext parent, int invokingState) {
+			super(parent, invokingState);
+		}
+		@Override public int getRuleIndex() { return RULE_f; }
+	 
+		public FContext() { }
+		public void copyFrom(FContext ctx) {
+			super.copyFrom(ctx);
+		}
+	}
+	@SuppressWarnings("CheckReturnValue")
+	public static class PassEContext extends FContext {
+		public EContext e() {
+			return getRuleContext(EContext.class,0);
+		}
+		public PassEContext(FContext ctx) { copyFrom(ctx); }
+	}
+	@SuppressWarnings("CheckReturnValue")
+	public static class NumContext extends FContext {
+		public TerminalNode NUM() { return getToken(GrammarParser.NUM, 0); }
+		public NumContext(FContext ctx) { copyFrom(ctx); }
+	}
+	@SuppressWarnings("CheckReturnValue")
+	public static class IdContext extends FContext {
+		public TerminalNode ID() { return getToken(GrammarParser.ID, 0); }
+		public IdContext(FContext ctx) { copyFrom(ctx); }
+	}
+
+	public final FContext f() throws RecognitionException {
+		FContext _localctx = new FContext(_ctx, getState());
+		enterRule(_localctx, 8, RULE_f);
+		try {
+			setState(47);
+			_errHandler.sync(this);
+			switch (_input.LA(1)) {
+			case T__5:
+				_localctx = new PassEContext(_localctx);
+				enterOuterAlt(_localctx, 1);
+				{
+				setState(41);
+				match(T__5);
+				setState(42);
+				e(0);
+				setState(43);
+				match(T__6);
+				}
+				break;
+			case ID:
+				_localctx = new IdContext(_localctx);
+				enterOuterAlt(_localctx, 2);
+				{
+				setState(45);
+				match(ID);
+				}
+				break;
+			case NUM:
+				_localctx = new NumContext(_localctx);
+				enterOuterAlt(_localctx, 3);
+				{
+				setState(46);
+				match(NUM);
+				}
+				break;
+			default:
+				throw new NoViableAltException(this);
+			}
+		}
+		catch (RecognitionException re) {
+			_localctx.exception = re;
+			_errHandler.reportError(this, re);
+			_errHandler.recover(this, re);
+		}
+		finally {
+			exitRule();
+		}
+		return _localctx;
+	}
+
+	public boolean sempred(RuleContext _localctx, int ruleIndex, int predIndex) {
+		switch (ruleIndex) {
+		case 2:
+			return e_sempred((EContext)_localctx, predIndex);
+		case 3:
+			return t_sempred((TContext)_localctx, predIndex);
+		}
+		return true;
+	}
+	private boolean e_sempred(EContext _localctx, int predIndex) {
+		switch (predIndex) {
+		case 0:
+			return precpred(_ctx, 2);
+		}
+		return true;
+	}
+	private boolean t_sempred(TContext _localctx, int predIndex) {
+		switch (predIndex) {
+		case 1:
+			return precpred(_ctx, 2);
+		}
+		return true;
+	}
+
+	public static final String _serializedATN =
+		"\u0004\u0001\n2\u0002\u0000\u0007\u0000\u0002\u0001\u0007\u0001\u0002"+
+		"\u0002\u0007\u0002\u0002\u0003\u0007\u0003\u0002\u0004\u0007\u0004\u0001"+
+		"\u0000\u0004\u0000\f\b\u0000\u000b\u0000\f\u0000\r\u0001\u0001\u0001\u0001"+
+		"\u0001\u0001\u0001\u0001\u0001\u0002\u0001\u0002\u0001\u0002\u0001\u0002"+
+		"\u0001\u0002\u0001\u0002\u0005\u0002\u001a\b\u0002\n\u0002\f\u0002\u001d"+
+		"\t\u0002\u0001\u0003\u0001\u0003\u0001\u0003\u0001\u0003\u0001\u0003\u0001"+
+		"\u0003\u0005\u0003%\b\u0003\n\u0003\f\u0003(\t\u0003\u0001\u0004\u0001"+
+		"\u0004\u0001\u0004\u0001\u0004\u0001\u0004\u0001\u0004\u0003\u00040\b"+
+		"\u0004\u0001\u0004\u0000\u0002\u0004\u0006\u0005\u0000\u0002\u0004\u0006"+
+		"\b\u0000\u0002\u0001\u0000\u0002\u0003\u0001\u0000\u0004\u00051\u0000"+
+		"\u000b\u0001\u0000\u0000\u0000\u0002\u000f\u0001\u0000\u0000\u0000\u0004"+
+		"\u0013\u0001\u0000\u0000\u0000\u0006\u001e\u0001\u0000\u0000\u0000\b/"+
+		"\u0001\u0000\u0000\u0000\n\f\u0003\u0002\u0001\u0000\u000b\n\u0001\u0000"+
+		"\u0000\u0000\f\r\u0001\u0000\u0000\u0000\r\u000b\u0001\u0000\u0000\u0000"+
+		"\r\u000e\u0001\u0000\u0000\u0000\u000e\u0001\u0001\u0000\u0000\u0000\u000f"+
+		"\u0010\u0005\b\u0000\u0000\u0010\u0011\u0005\u0001\u0000\u0000\u0011\u0012"+
+		"\u0003\u0004\u0002\u0000\u0012\u0003\u0001\u0000\u0000\u0000\u0013\u0014"+
+		"\u0006\u0002\uffff\uffff\u0000\u0014\u0015\u0003\u0006\u0003\u0000\u0015"+
+		"\u001b\u0001\u0000\u0000\u0000\u0016\u0017\n\u0002\u0000\u0000\u0017\u0018"+
+		"\u0007\u0000\u0000\u0000\u0018\u001a\u0003\u0006\u0003\u0000\u0019\u0016"+
+		"\u0001\u0000\u0000\u0000\u001a\u001d\u0001\u0000\u0000\u0000\u001b\u0019"+
+		"\u0001\u0000\u0000\u0000\u001b\u001c\u0001\u0000\u0000\u0000\u001c\u0005"+
+		"\u0001\u0000\u0000\u0000\u001d\u001b\u0001\u0000\u0000\u0000\u001e\u001f"+
+		"\u0006\u0003\uffff\uffff\u0000\u001f \u0003\b\u0004\u0000 &\u0001\u0000"+
+		"\u0000\u0000!\"\n\u0002\u0000\u0000\"#\u0007\u0001\u0000\u0000#%\u0003"+
+		"\b\u0004\u0000$!\u0001\u0000\u0000\u0000%(\u0001\u0000\u0000\u0000&$\u0001"+
+		"\u0000\u0000\u0000&\'\u0001\u0000\u0000\u0000\'\u0007\u0001\u0000\u0000"+
+		"\u0000(&\u0001\u0000\u0000\u0000)*\u0005\u0006\u0000\u0000*+\u0003\u0004"+
+		"\u0002\u0000+,\u0005\u0007\u0000\u0000,0\u0001\u0000\u0000\u0000-0\u0005"+
+		"\b\u0000\u0000.0\u0005\t\u0000\u0000/)\u0001\u0000\u0000\u0000/-\u0001"+
+		"\u0000\u0000\u0000/.\u0001\u0000\u0000\u00000\t\u0001\u0000\u0000\u0000"+
+		"\u0004\r\u001b&/";
+	public static final ATN _ATN =
+		new ATNDeserializer().deserialize(_serializedATN.toCharArray());
+	static {
+		_decisionToDFA = new DFA[_ATN.getNumberOfDecisions()];
+		for (int i = 0; i < _ATN.getNumberOfDecisions(); i++) {
+			_decisionToDFA[i] = new DFA(_ATN.getDecisionState(i), i);
+		}
+	}
+}
