410a697c3eb5156ea45f47c0dc5b533c8ca83072
Luis Espino <66042898+luisespino@users.noreply.github.com>
2025-08-25 22:18:47 -0600

    update figure 6.19

---
+++ figure-6-19/flex-bison-c/attr.h
@@ -0,0 +1,9 @@
+#ifndef ATTR_H
+#define ATTR_H
+
+typedef struct {
+    int addr;
+    char* code;
+} Attr;
+
+#endif
---
+++ figure-6-19/flex-bison-c/run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+bison -d parser.y
+flex lexer.l
+gcc -o run parser.tab.c lex.yy.c main.c -lfl
+./run
+rm parser.tab.c parser.tab.h lex.yy.c run
